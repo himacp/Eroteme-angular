5784a9d01dbcc600751435b38c087ee226895592
hima <himacp2396@gmail.com>
2019-02-17 22:46:35 +0530

    edited question component

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -8,51 +8,111 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionsComponent implements OnInit 
 {
 
-  public count2 : number = 0; 
+  public count1 : number = 0; 
+  public count2 : number= 0; 
   public count3 : number= 0; 
   public count4 : number= 0; 
-  public count5 : number= 0; 
-  public time = "12/09/2018";
+  public count5: number=0;
+  public count6: number=0;
+  public count7: number=0;
+  public count8: number=0;
+  public count9: number=0;
+  public count: number=0;
+
+  public time = "18/02/2018";
   checked = false;
   checked1 = false;
   constructor() { }
   
   upvote1(){
-    if(this.count3==0 && this.count2==0)
+    if(this.count2==0 && this.count1==0)
     {
-      this.count2 = 1;
+      this.count1 = 1;
     }
     else{
-      this.count2 =0;
+      this.count1 =0;
     }
     
   }
   downvote1(){
-    if (this.count2==0 && this.count3==0){
-      this.count3 = 1;
+    if (this.count1==0 && this.count2==0){
+      this.count2 = 1;
     }
     else{
-      this.count3 = 0;
+      this.count2 = 0;
     }
       
   }
   upvote2(){
-    if(this.count5==0 && this.count4==0)
+    if(this.count4==0 && this.count3==0)
     {
-      this.count4 = 1;
+      this.count3 = 1;
     }
     else{
-      this.count4 =0;
+      this.count3 =0;
     }
   }
   downvote2(){
-    if (this.count4==0 && this.count5==0){
+    if (this.count3==0 && this.count4==0){
+      this.count4 = 1;
+    }
+    else{
+      this.count4 = 0;
+    }
+  }
+
+  upvote3(){
+    if(this.count6==0 && this.count5==0)
+    {
       this.count5 = 1;
     }
     else{
-      this.count5 = 0;
+      this.count5 =0;
     }
   }
+    downvote3(){
+      if (this.count5==0 && this.count6==0){
+        this.count6 = 1;
+      }
+      else{
+        this.count6 = 0;
+      }
+    }
+    upvote4(){
+      if(this.count8==0 && this.count7==0)
+      {
+        this.count7 = 1;
+      }
+      else{
+        this.count7 =0;
+      }
+    }
+    downvote4(){
+      if (this.count7==0 && this.count8==0){
+        this.count8 = 1;
+      }
+      else{
+        this.count8 = 0;
+      }
+    }
+    upvote5(){
+      if(this.count==0 && this.count9==0)
+      {
+        this.count9 = 1;
+      }
+      else{
+        this.count9 =0;
+      }
+    }
+    downvote5(){
+      if (this.count9==0 && this.count==0){
+        this.count = 1;
+      }
+      else{
+        this.count = 0;
+      }
+    }
+
   ngOnInit() {
   }
 
